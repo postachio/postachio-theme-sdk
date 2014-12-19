3dbafcdbbef42c544959f3c125e3e83b3104db18
Shawn Adrian <shawn@nerdburn.com>
2014-12-19 15:36:26 -0800

    updating logging and routing

--- routes.js
+++ routes.js
@@ -4,25 +4,25 @@ module.exports = function(app) {
 
   app.route('/')
   .get(function(req, res) {
-    res.render('index', content('home'));  
+    res.render('theme', content('home'));  
   });
 
   app.route('/:slug')
   .get(function(req, res){
-    console.log('Called post route.');  
-    res.render('index', content('post'));      
+    console.log('Called post route: /:slug');  
+    res.render('theme', content('post'));      
   });                    
   
   app.route('/tag/:tag')
   .get(function(req, res){
-    console.log('Called /tag/ route.');
-    res.render('index', content('tag'));
+    console.log('Called tag route: /tag/:tag');
+    res.render('theme', content('tag'));
   });
 
   app.route('/page/:slug')
   .get(function(req, res){
-    console.log('Called /page/ route.');  
-    res.render('index', content('page'));
+    console.log('Called page route: /page/:slug');
+    res.render('theme', content('page'));
   });
   
 };
